87468ec09ebd097553b3ceb76f72ff43cd4e72bd
Andreas Blaabjerg <Andreas.blaabjerg1@hotmail.com>
2020-08-18 10:48:12 +0200

    1/3

---
+++ Code_Presentations/07+08_CrossAppXam/XamFormsDemoX/XamFormsDemo/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using XamFormsDemo.Services;
+using XamFormsDemo.Views;
+
+namespace XamFormsDemo
+{
+    public partial class App : Application
+    {
+
+        public App()
+        {
+            InitializeComponent();
+
+            DependencyService.Register<Place>();
+            MainPage = new AppShell();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ Code_Presentations/07+08_CrossAppXam/XamFormsDemoX/XamFormsDemo/Views/Homepage.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using XamFormsDemo.Models;
+using XamFormsDemo.Services;
+
+namespace XamFormsDemo.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Homepage : ContentPage
+    {
+        Place mock => DependencyService.Get<Place>();
+
+        public Homepage()
+        {
+            InitializeComponent();
+            getData();
+        }
+
+        public async void getData()
+        {
+            List<PlaceModel> newPlace = new List<PlaceModel>();
+
+            newPlace = await mock.getPlaces();
+
+            myPlaces.ItemsSource = newPlace;
+        }
+
+        private void MyPlaces_OnSelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            //caster value som Placemodel
+            PlaceModel myValue = e.CurrentSelection.FirstOrDefault() as PlaceModel;
+
+            Application.Current.MainPage.Navigation.PushAsync(new MapPage(myValue));
+
+        }
+    }
+}
