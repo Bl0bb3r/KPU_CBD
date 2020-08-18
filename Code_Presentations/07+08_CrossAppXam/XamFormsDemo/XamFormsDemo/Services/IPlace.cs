8e0c45f400b429676bc1a8d31331d086276bfc8c
Andreas Blaabjerg <Andreas.blaabjerg1@hotmail.com>
2020-08-18 11:40:02 +0200

    moved folder

---
+++ Code_Presentations/07+08_CrossAppXam/XamFormsDemo/XamFormsDemo/Services/IPlace.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using XamFormsDemo.Models;
+
+namespace XamFormsDemo.Services
+{
+    public interface IPlace
+    {
+        Task<List<PlaceModel>> getPlaces();
+
+    }
+}
